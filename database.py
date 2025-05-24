d7d75e7a0d9446f755038f38afce53834ea887cb
Channa Dissanayaka <ywchanna@gmail.com>
2025-05-24 16:51:42 +0530

    modified

--- database.py
+++ database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = ''
+URL_DATABASE = 'mysql+pymysql://root:root@localhost:3306/BlogAppliction'
+# mysql+pymysql://<username>:<password>@<host>:<port>/<database_name>
+
 
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
